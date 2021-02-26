7e0a2708b264b47f12deebd543b40e3d6fb2a7f9
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2021-02-26 18:07:02 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dueling Diplomats
+Rust/WebAssembly real-time, tactical battlefield strategy game
+
+
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(start)]
+pub fn runner() {
+    // Setup panic hook
+    utils::set_panic_hook();
+
+    let window = web_sys::window().expect("Expected global `window` object!");
+    window.alert_with_message("Hello, World!").unwrap();
+}
